9ea859d10cb0d94ee798131489eba2d8935a3eda
Adrian Wu <eattofueveryday@gmail.com>
2026-04-11 15:57:49 -0400

    Finish rpc and controller

--- controller.ts
+++ controller.ts
@@ -1,50 +1,214 @@
 import { Socket } from "net"
+import { RobotRpc, RpcStatus, TransferOps } from "./rpc";
+
+export type InventoryConfig = {
+  x: number,
+  z: number,
+  side: Side,
+  slots: number[]
+};
+
+export type MachineConfig = {
+  inputInventory: InventoryConfig,
+  outputInventory: InventoryConfig
+};
 
 export type RobotControllerConfig = {
-  machines: ({
-    x: number,
-    y: number,
-    side: Side,
-    id: string
-  })[]
+  machines: { [id: string]: MachineConfig },
+  aeItemInterface: InventoryConfig,
+  robotSlots: number,
+  robotTanks: number
 };
 
 export type JobDef = {
   machineId: string,
+  expectedTicks: number,
   ingredients: ItemStack[],
   results: ItemStack[],
 };
 
-export type Job = {
-  def: JobDef,
-  expectedCompletionTime: number | null, // null if not being completed
+export enum JobStatus {
+  Dispatched,
+  MissingIngredients,
+  MissingMachine,
+  Running
 };
 
+export type Job = {
+  jobId: number,
+  def: JobDef,
+  status: JobStatus,
+  currentMachineIndex: number | null,
+  expectedCompletionTime: number | null,
+  callback: (err?: string) => void
+};
+
+async function asyncSeqFilter<T>(arr: T[], cb: (t: T) => Promise<boolean>): Promise<T[]> {
+  const filter: boolean[] = [];
+  for(let i = 0; i < arr.length; i++) {
+    filter.push(await cb(arr[i]));
+  }
+  return arr.filter((v, i) => filter[i]);
+}
+
 export class RobotController {
-  conn: Socket;
+  rpc: RobotRpc;
   config: RobotControllerConfig;
 
-  queuedJobs: Job[];
+  nextJobId: number;
+  jobs: Job[];
   currentX: number;
-  currentY: number;
+  currentZ: number;
+  ticking: boolean;
+  machineUsed: { [x: string]: boolean }
 
-  constructor(conn: Socket, config: RobotControllerConfig) {
-    this.conn = conn;
+  constructor(rpc: RobotRpc, config: RobotControllerConfig) {
+    this.rpc = rpc;
     this.config = config;
 
-    this.queuedJobs = [];
+    this.nextJobId = 1;
+    this.jobs = [];
     this.currentX = 0;
-    this.currentY = 0;
+    this.currentZ = 0;
+    this.ticking = false;
+    this.machineUsed = {};
   }
 
-  submit(job: JobDef) {
-    this.queuedJobs.push({
+  submit(job: JobDef, callback: (err?: string) => void): number {
+    const jobId = this.nextJobId++;
+    this.jobs.push({
+      jobId: jobId,
       def: job,
-      expectedCompletionTime: null
+      status: JobStatus.Dispatched,
+      currentMachineIndex: null,
+      expectedCompletionTime: null,
+      callback: callback
+    });
+    return jobId;
+  }
+  
+  submitAsync(job: JobDef): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.submit(job, (err) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
   }
 
   jobQueue(): Job[] {
-    return this.queuedJobs;
+    return this.jobs;
+  }
+
+  async moveRobot(targetX: number, targetZ: number) {
+    const moveX = async (x: number): Promise<void> => {
+      await this.rpc.move(x > 0 ? Side.EAST : Side.WEST, Math.abs(x));
+    };
+    const moveZ = async (z: number): Promise<void> => {
+      await this.rpc.move(z > 0 ? Side.SOUTH : Side.NORTH, Math.abs(z));
+    };
+    if(targetZ == this.currentZ) {
+      await moveZ(targetZ - this.currentZ);
+    } else {
+      // move to Z = 0
+      await moveZ(-this.currentZ);
+      // move to targetX
+      await moveX(targetX - this.currentX);
+      // move to targetZ
+      await moveZ(targetZ);
+    }
+    this.currentX = targetX;
+    this.currentZ = targetZ;
+  }
+
+  async tick() {
+    if(this.ticking) return;
+    this.ticking = true;
+    this.jobs = await asyncSeqFilter(this.jobs, async job => {
+      if(job.status === JobStatus.Dispatched || job.status === JobStatus.MissingIngredients || job.status === JobStatus.MissingMachine) {
+        // attempt to schedule
+        if(this.machineUsed[job.def.machineId]) {
+          job.status = JobStatus.MissingMachine;
+          return true;
+        }
+
+        // try to get ingredients
+        const res = await this.moveRobot(this.config.aeItemInterface.x, this.config.aeItemInterface.z);
+        const proms = job.def.ingredients.map((v, i) => {
+          return this.rpc.transfer(
+            TransferOps.ItemAeToSelf,
+            this.config.aeItemInterface.side,
+            0, i,
+            v.amount,
+            v.id,
+            v.meta
+          );
+        });
+
+        // return ingredients if they're not all present
+        const responses = await Promise.all(proms);
+        if(responses.some(v => v.status !== RpcStatus.Ok)) {
+          await Promise.all(job.def.ingredients.map((v, i) =>
+            this.rpc.transfer(
+              TransferOps.ItemSelfToAe,
+              this.config.aeItemInterface.side,
+              i, 0,
+              v.amount,
+              v.id,
+              v.meta
+            )));
+          job.status = JobStatus.MissingIngredients;
+          return true;
+        }
+
+        // put in the ingredients
+        const machineCfg = this.config.machines[job.def.machineId];
+        await this.moveRobot(machineCfg.inputInventory.x, machineCfg.inputInventory.z);
+        await Promise.all(job.def.ingredients.map((v, i) => 
+          this.rpc.transfer(
+            TransferOps.ItemSelfToMachine,
+            this.config.aeItemInterface.side,
+            i, machineCfg.inputInventory.slots[i],
+            v.amount,
+            v.id,
+            v.meta
+          )));
+        job.status = JobStatus.Running;
+        job.expectedCompletionTime = Date.now() + 50 * job.def.expectedTicks;
+        return true;
+      } else if(job.status === JobStatus.Running && job.expectedCompletionTime && job.expectedCompletionTime < Date.now()) {
+        // collect results
+        const machineCfg = this.config.machines[job.def.machineId];
+        await this.moveRobot(machineCfg.outputInventory.x, machineCfg.outputInventory.z);
+        const responses = await Promise.all(job.def.results.map((v, i) => 
+          this.rpc.transfer(
+            TransferOps.ItemMachineToSelf,
+            machineCfg.outputInventory.side,
+            machineCfg.outputInventory.slots[i], i,
+            v.amount,
+            v.id,
+            v.meta
+          )));
+        if(responses.some(v => v.status !== RpcStatus.Ok)) {
+          console.log("control: warn: collecting item resulted in err");
+        }
+        await this.moveRobot(machineCfg.outputInventory.x, machineCfg.outputInventory.z);
+        await Promise.all(job.def.results.map((v, i) => {
+          this.rpc.transfer(
+            TransferOps.ItemSelfToAe,
+            this.config.aeItemInterface.side,
+            i, 0,
+            v.amount,
+            v.id,
+            v.meta
+          )
+        }));
+      }
+      return true;
+    });
+    this.ticking = false;
   }
 }
---
+++ rpc.ts
@@ -0,0 +1,111 @@
+import { Socket } from "net";
+import { createInterface, Interface } from "readline";
+
+enum RpcOpcodes {
+  Reset = 0x1,
+  Move = 0x2,
+  Transfer = 0x3
+}
+
+type RpcResponse<T> = { status: RpcStatus, data: T | null };
+
+function validateData<T>(data: unknown[] | null, types: string[]): T | null {
+  if(data === null) return null;
+  data.forEach((v, i) => {
+    if(types[i] !== typeof v) throw new Error("Invalid response type at index " + i);
+  });
+  return data as T;
+}
+
+function validateRpcData<T>(data: RpcResponse<unknown[]>, types: string[]): RpcResponse<T> {
+  if(data.status !== RpcStatus.Ok) {
+    throw new Error("Robot returned error " + data.status);
+  }
+  return {
+    status: data.status,
+    data: validateData(data.data, types)
+  }
+}
+
+export enum RpcStatus {
+  Ok = 0x1,
+  ErrUnexpectedItem = 0x2,
+  ErrCannotMove = 0x3,
+  ErrNoInventory = 0x4,
+  ErrMissingItems = 0x5
+}
+
+export enum TransferOps {
+  ItemSelfToMachine = 0x0,
+  ItemMachineToSelf = 0x1,
+  ItemSelfToAe = 0x2,
+  ItemAeToSelf = 0x3,
+  FluidSelfToMachine = 0x4,
+  FluidMachineToSelf = 0x5,
+  FluidSelfToAe = 0x6,
+  FluidAeToSelf = 0x7
+}
+
+export class RobotRpc {
+  conn: Socket;
+  readline: Interface;
+  nextSid: number;
+  inflightReqs: { [x: number]: (res: RpcResponse<unknown[]>) => void };
+
+  constructor(conn: Socket) {
+    this.conn = conn;
+    this.nextSid = 1;
+    this.inflightReqs = {};
+    this.readline = createInterface(conn);
+    this.readline.on("line", line => {
+      try {
+        this.processRes(line);
+      } catch(err) {}
+    });
+  }
+
+  processRes(line: string) {
+    const data = line.split(",");
+    const sid = parseInt(data[0]);
+    const status = parseInt(data[1]);
+    const data2 = data.slice(3);
+    if(!this.inflightReqs[sid]) {
+      console.log("rpc: warn: unmatched sid " + sid);
+    } else {
+      this.inflightReqs[sid]({
+        status: status,
+        data: data2
+      });
+      delete this.inflightReqs[sid];
+    }
+  }
+
+  rpc(opcode: number, args: unknown[]): Promise<RpcResponse<unknown[]>> {
+    return new Promise((resolve, _) => {
+      const sid = this.nextSid++;
+      args.forEach(v => {
+        if(typeof v === "number") {
+        } else if(typeof v === "string") {
+          if(v.includes(",")) throw new Error("Argument contains comma");
+        } else {
+          throw new Error("Only strings and numbers allowed in argument list");
+        }
+      });
+      const requestStr = `${sid},${opcode},${args.toString()}`;
+      this.conn.write(Buffer.from(requestStr, "utf8"));
+      this.inflightReqs[sid] = resolve;
+    });
+  }
+
+  async reset(): Promise<RpcResponse<[]>> {
+    return validateRpcData<[]>(await this.rpc(RpcOpcodes.Reset, []), []);
+  }
+
+  async move(side: Side, blocks: number): Promise<RpcResponse<[]>> {
+    return validateRpcData<[]>(await this.rpc(RpcOpcodes.Move, [side, blocks]), []);
+  }
+
+  async transfer(subOp: TransferOps, side: Side, srcSlot: number, dstSlot: number, amount: number, expectId: string, expectMeta: number): Promise<RpcResponse<[]>> {
+    return validateRpcData<[]>(await this.rpc(RpcOpcodes.Transfer, [subOp, side, srcSlot, dstSlot, amount, expectId, expectMeta]), []);
+  }
+}
