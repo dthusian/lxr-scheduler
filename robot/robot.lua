1b37d78c1b8881d43846d8f5c48b77cf29cb3af3
Adrian Wu <eattofueveryday@gmail.com>
2026-04-19 11:59:29 -0400

    Reorganize lua

--- robot/robot.lua
+++
@@ -1,8 +0,0 @@
-local robot = require("robot")
-local component = require("component")
-local ic = component.inventory_controller
-
-for k,v in pairs(ic.getStackInInternalSlot(1)) do
-    print(k .. ", " .. tostring(v))
-end
-
