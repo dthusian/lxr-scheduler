d67e7a11873fd852d1e9f45682364c38128dd38e
Adrian Wu <eattofueveryday@gmail.com>
2026-04-19 01:47:18 -0400

    Add aecontrol and testbench

--- robot/aecontrol.lua
+++ robot/aecontrol.lua
@@ -1,6 +1,9 @@
 local Socketw = require("lxsocketw")
 local sock = Socketw.new()
 
+local db = component.database
+local me = component.me_interface
+
 function split(input, regex)
     local entries = {}
     for str in string.gmatch(input, regex) do
@@ -16,9 +19,54 @@ function sock:on_message(msg)
     local opcode = tonumber(entries[2])
     print("sid: " .. sid .. "; opcode: " .. opcode)
 
-    if opcode == 0x1 then
-        -- provide item
-    elseif opcode == 0x2 then
-        -- provide fluid
+    local status, err = pcall(function()
+        if opcode == 0x4 then
+            -- provide item
+            local len = tonumber(entries[3])
+            for i=1,len do
+                db.clear(1)
+                local id = entries[i * 2 + 2]
+                local meta = tonumber(entries[i * 2 + 3])
+                if (not db.set(1, id, meta, "")) or (not me.setInterfaceConfiguration(i, db.address, 1, 64)) then
+                    sock:send(sid .. ",6")
+                    return
+                end
+            end
+            sock:send(sid .. ",1")
+        elseif opcode == 0x5 then
+            -- provide fluid
+            local len = tonumber(entries[3])
+            for i=1,len do
+                db.clear(1)
+                local id = entries[i + 3]
+                -- there appears to be no better way to do this unfortunately
+                -- 1) find fluid in network and get its label
+                local fluids = me.getFluidsInNetwork()
+                local label = nil
+                for i, v in ipairs(fluids) do
+                    if v.name == id then
+                        label = v.label
+                        break
+                    end
+                end
+                if label == nil then
+                    -- fluid not found
+                    sock:send(sid .. ",5")
+                    return
+                end
+                -- 2) use that label to store the associated fluid drop into a db.
+                me.store({ label = "drop of " .. label }, db.address, 1, 1)
+                -- 3) set interface config with that fluid drop
+                if not me.setFluidInterfaceConfiguration(i, db.address, 1) then
+                    sock:send(sid .. ",6")
+                    return
+                end
+            end
+            sock:send(sid .. ",1")
+        end
+    end)
+    
+    if status then
+        sock:send(sid .. ",6")
     end
 end
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createServer } from "net";
+
+createServer(sock => {
+  console.log("got connection, sending commands...");
+  sock.write("4,111,3,gregtech:gt.metaitem.01,11028,gregtech:gt.metaitem.01,32351,gregtech:gt.integrated_circuit,2\n");
+  sock.write("5,222,2,molten.rubber,water");
+  sock.on("data", data => {
+    console.log("response: " + data + "\n");
+  });
+  sock.on("close", () => process.exit(0));
+}).listen(3001)
